180ce21f48847e11a57372dd0d1ccacba38ab3de
Berkay Aydın <99977526+ValyrianAtreides@users.noreply.github.com>
2025-12-04 22:42:36 +0300

    vercel.json added

--- src/generated/prisma/wasm.js
+++ src/generated/prisma/wasm.js
@@ -208,7 +208,7 @@ const config = {
     "isCustomOutput": true
   },
   "relativeEnvPaths": {
-    "rootEnvPath": null,
+    "rootEnvPath": "../../../.env",
     "schemaEnvPath": "../../../.env"
   },
   "relativePath": "../../../prisma",
