b3160f9fb96c76dc53c0f04b3ef26bc1bbf9ab7f
Berkay Aydın <99977526+ValyrianAtreides@users.noreply.github.com>
2025-12-04 21:59:15 +0300

    generated fixes

--- src/db/prisma.ts
+++ src/db/prisma.ts
@@ -1,11 +1,19 @@
 import { PrismaClient } from '@/generated/prisma/client';
+import { PrismaPg } from '@prisma/adapter-pg';
+
+const adapter = new PrismaPg({ 
+  connectionString: process.env.DATABASE_URL 
+});
+
+const prismaClientSingleton = () => {
+  return new PrismaClient({ adapter })
+}
 
 const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClient | undefined;
+  prisma: ReturnType<typeof prismaClientSingleton> | undefined;
 };
 
-//use the existing prisma client if not exists then create new one
-export const prisma = globalForPrisma.prisma ?? new PrismaClient({} as any);
+export const prisma = globalForPrisma.prisma ?? prismaClientSingleton();
 
 if (process.env.NODE_ENV !== 'production') {
   globalForPrisma.prisma = prisma;
